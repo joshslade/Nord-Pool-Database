5bce46533f0119ea5f92d8d07c3c7cad0299743d
joshslade <55036535+joshslade@users.noreply.github.com>
2025-07-15 13:36:38 +0100

    Add new view for combined analytics data

--- schemas/analytics-view_transforms.sql
+++ schemas/analytics-view_transforms.sql
@@ -116,3 +116,46 @@ group by
     ,dfh.delivery_start
     ,dfh.delivery_end
 order by dfh.delivery_start;
+
+CREATE OR REPLACE VIEW analytics.latest_da_combined AS
+SELECT
+    p.delivery_date,
+    p.time_window_gmt,
+    p.delivery_start,
+    p.delivery_end,
+    p.uk_gbp,
+    p.no2_gbp,
+    ip.uk_gbp AS uk_idx_gbp,
+    ip.no2_gbp AS no2_idx_gbp,
+    v.no2_buy_mwh,
+    v.no2_sell_mwh,
+    v.uk_buy_mwh,
+    v.uk_sell_mwh,
+    c.no2_to_uk_mwh,
+    c.uk_to_no2_mwh,
+    f.no2_total_export_mwh,
+    f.no2_total_import_mwh,
+    f.uk_total_export_mwh,
+    f.uk_total_import_mwh
+FROM analytics.latest_da_prices p
+LEFT JOIN analytics.latest_da_idx_prices ip
+    ON p.delivery_date = ip.delivery_date
+    AND p.time_window_gmt = ip.time_window_gmt
+    AND p.delivery_start = ip.delivery_start
+    AND p.delivery_end = ip.delivery_end
+LEFT JOIN analytics.latest_da_volumes v
+    ON p.delivery_date = v.delivery_date
+    AND p.time_window_gmt = v.time_window_gmt
+    AND p.delivery_start = v.delivery_start
+    AND p.delivery_end = v.delivery_end
+LEFT JOIN analytics.latest_da_capacity c
+    ON p.delivery_date = c.delivery_date
+    AND p.time_window_gmt = c.time_window_gmt
+    AND p.delivery_start = c.delivery_start
+    AND p.delivery_end = c.delivery_end
+LEFT JOIN analytics.latest_da_flow f
+    ON p.delivery_date = f.delivery_date
+    AND p.time_window_gmt = f.time_window_gmt
+    AND p.delivery_start = f.delivery_start
+    AND p.delivery_end = f.delivery_end
+ORDER BY p.delivery_start;
